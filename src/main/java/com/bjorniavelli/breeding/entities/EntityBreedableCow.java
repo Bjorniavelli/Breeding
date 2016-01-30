371c6e5a0f2b9470fb9aa3a16a10dc9c7af266e4
Bjorniavelli Bjorniavelli <Bjorniavelli Bjorniavelli>
2016-01-30 14:32:27 -0600

    BreedableCow class

---
+++ src/main/java/com/bjorniavelli/breeding/entities/EntityBreedableCow.java
@@ -0,0 +1,12 @@
+package com.bjorniavelli.breeding.entities;
+
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.world.World;
+
+public class EntityBreedableCow extends EntityCow
+{
+    public EntityBreedableCow (World worldIn)
+    {
+        super (worldIn);
+    }
+}
